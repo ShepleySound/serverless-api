c4f9bc45eb1d105c48006baab3d4f20b8a2d1ac9
Robert Shepley <shepleysound@gmail.com>
2022-10-12 20:08:00 -0700

    Add exports.handler to lambda functions.

--- lambdaGet/index.js
+++ lambdaGet/index.js
@@ -1,3 +1,4 @@
-'use strict';
-
-console.log('initialized 6')
+exports.handler =  async function(event, context) {
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+  return context.logStreamName
+}

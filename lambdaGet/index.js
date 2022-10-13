4c8f0bb0d6e6eebd5f34cc5fb5a767e2d3f36c10
Robert Shepley <shepleysound@gmail.com>
2022-10-12 21:59:22 -0700

    Update lambda functions.

--- lambdaGet/index.js
+++ lambdaGet/index.js
@@ -2,17 +2,30 @@ const AWS = require('aws-sdk');
 const dynamo = new AWS.DynamoDB.DocumentClient();
 
 exports.handler =  async function(event, context) {
-  let body;
-  let statusCode = 200;
-  if (event.pathParameters !== null) {
-    body = await dynamo.get({
-      TableName: 'customers',
-      Key: {
-        id: event.pathParameters.id
-      }
-    }).promise();
-  } else {
-    body = await dynamo.scan({ TableName: 'customers' }).promise();
+    let statusCode = 200;
+    let body;
+    let headers = {
+      "Content-Type": "application/json"
+    }
+  try {
+    if (event.pathParameters) {
+      body = await dynamo
+      .get({
+        TableName: 'customers',
+        Key: {
+          id: event.pathParameters.id
+        }
+      })
+      .promise();
+    } else {
+      body = await dynamo.scan({ TableName: 'customers' }).promise();
+    }
+  } catch(error) {
+    console.log(error);
+    statusCode = 400;
+    body = error.message;
+  } finally {
+    body = JSON.stringify(body)
   }
-  return { statusCode, body }
+  return { statusCode, body, headers }
 }
--- lambdaPut/index.js
+++ lambdaPut/index.js
@@ -4,15 +4,28 @@ const dynamo = new AWS.DynamoDB.DocumentClient();
 exports.handler =  async function(event, context) {
   let body;
   let statusCode = 200;
-  let request = JSON.parse(event.body);
-  await dynamo.put({
-    TableName: 'customers',
-    Item: {
-      id: request.id,
-      name: request.name,
-      purchaseCount: request.purchaseCount,
-    }
-  }).promise();
+  let headers = {
+    "Content-Type": "application/json"
+  }
+  try {
+    let request = JSON.parse(event.body);
+    await dynamo
+    .post({
+      TableName: 'customers',
+      Item: {
+        id: request.id,
+        name: request.name,
+        purchaseCount: request.purchaseCount,
+      }
+    })
+    .promise();
+  } catch(error) {
+    console.log(error);
+    statusCode = 400;
+    body = error.message;
+  } finally {
+    body = JSON.stringify(body)
+  }
   body = `Updated customer ${request.id}`
-  return { statusCode, body }
+  return { statusCode, body, headers }
 }

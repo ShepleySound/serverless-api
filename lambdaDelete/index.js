9be2fb6b53c53235e8e3feb9f09b3e04403065c6
Robert Shepley <shepleysound@gmail.com>
2022-10-12 21:18:48 -0700

    Write CRUD functions using basic DynamoDB accessors.

--- lambdaDelete/index.js
+++ lambdaDelete/index.js
@@ -1,4 +1,15 @@
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
 exports.handler =  async function(event, context) {
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-  return context.logStreamName
+  let body;
+  let statusCode = 200;
+  await dynamo.delete({
+    TableName: 'customers',
+    Key: {
+      id: event.pathParameters.id,
+    }
+  }).promise();
+  body = `Updated customer ${request.id}`
+  return { statusCode, body }
 }
